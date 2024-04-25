234beaacdfb38b6504a3ef3625485f7ead3deb5b
ROHIT KUMAR <21mc3013@rgipt.ac.in>
2024-04-25 23:28:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3.9
 from flask import Flask, render_template, request, jsonify
 from selenium import webdriver
-from schedule_auth import ScheduleAuth
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.edge.service import Service
+import warnings
+
+warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
@@ -8,15 +14,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/api/acceptRequest', methods=['POST'])
-def acceptRequest():
-    data = request.get_json()
-    print(data)
-    return jsonify({'status': 'success'})
-
-@app.route('/api/validate')
+@app.route('/api/validate', methods=['POST'])
 def validate():
-    pass
+    data = request.json
+    edge_options = EdgeOptions()
+    edge_options.use_chromium = True  # Use Chromium Edge
+    edge_options.add_argument("--headless")  # Enable headless mode
+    msedgedriver_path = "msedgedriver.exe"
+    edge_service = Service(msedgedriver_path)
+    browser = webdriver.Edge(service=edge_service, options=edge_options)
+    browser_name = "Edge"
+    browser.get("http://192.168.100.1:8090/httpclient.html")
+    # Find and fill form elements
+    input_field = browser.find_element(By.NAME, "username")
+    input_field.send_keys(data['username'])
+    input_field = browser.find_element(By.NAME, "password")
+    input_field.send_keys(data['password'])
+    # Submit the form
+    button_row = browser.find_element(By.ID, "loginbutton")
+    button_row.click()
+    return jsonify({"status": "success"})
 
 @app.route('/api/scheduleAuth')
 def scheduleAuth():
